658ba88cd017ad78849df1de8d64fd34b7bfe159
rjhoppe-W <48058894+rjhoppe@users.noreply.github.com>
2024-02-16 12:23:30 -0500

    feat: Pushing file validation logic to main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 import pandas as pd
+import time
 import os
 import yaml
 import asyncio
@@ -25,45 +26,59 @@ st.set_page_config(
 def calc_number_rows(df):
   num_of_rows = len(df)
   if num_of_rows > 200:
-    st.write("Distribution list exceeds 100 respondents. Please reduce the list or reach out to an admin.")
+    st.write("❌ Distribution list exceeds 100 respondents. Please reduce the list or reach out to an admin.")
   else:
     return num_of_rows
 
 async def check_file(df, column_validation):
-  if df.columns.tolist() != ['client_name','url','message','phone_numbers']:
-    st.write('Column headers incorrect. Check spelling and order.')
-  else:
-    st.write('Column headers formatted correctly.')
-    column_validation = True
-    return column_validation
+  try:
+    async with asyncio.timeout(10):
+      if df.columns.tolist() != ['client_name','url','message','phone_numbers']:
+        st.warning('❌ Column headers incorrect. Check spelling and order.')
+      elif len(df['client_name']) > 1:
+        st.warning('❌ Client column only requires one value. See example file.')
+      elif len(df['url']) > 1:
+        st.warning('❌ URL column only requires one value. See example file.')
+      elif len(df['message']) > 1:
+        st.warning('❌ Message column only requires one value. See example file.')
+      else:
+        st.write('✅ Column headers formatted correctly.')
+        column_validation = True
+        await asyncio.sleep(1)
+        return column_validation
+  except TimeoutError:
+    st.warning('❌ Application timeout, try again later.')
   
 async def parse_msg_data(df, content_validation):
   message = TextBlob(df[['message']].values[0][0])
   if message != str(df[['message']].values[0][0]):
-    st.write('Spelling errors detected in message.')
+    st.write('❌ Spelling errors detected in message.')
     st.write(f'Recommended spelling: {message.correct()}')
   else:
-    st.write('Message content spellchecked.')
+    st.write('✅ Message content spellchecked.')
     content_validation = True
+    await asyncio.sleep(1)
     return content_validation
 
 async def parse_valid_numbers(df, phone_num_validation):
   for i, v in enumerate(df['phone_numbers']):
     v = len(str(v))
     if v != 11:
-       st.write(f'Invalid phone number at row: {i}')
+       st.write(f'❌ Invalid phone number at row: {i}')
     else:
-      st.write('Phone numbers validated.')
+      st.write('✅ Phone numbers validated.')
   phone_num_validation = True
+  await asyncio.sleep(1)
   return phone_num_validation
 
 async def parse_dup_numbers(df, dedupe_validation, num_of_rows):
-  if set(df['phone_numbers']) != num_of_rows:
-    st.write('Duplicate phone numbers detected.')
+  if len(set(df['phone_numbers'])) > 1 and set(df['phone_numbers']) != num_of_rows:
+    st.warning('❌ Duplicate phone numbers detected.')
     return
   else:
-    st.write('No duplicates detected')
+    st.write('✅ No duplicates detected.')
     dedupe_validation = True
+    await asyncio.sleep(1)
     return dedupe_validation
   
 async def distribute_sms(df):
@@ -99,26 +114,23 @@ async def main():
                   label_visibility='hidden')
   st.caption('The .csv file must have the following column headers: client_name, url, message, phone_numbers')
   if uploaded_file is not None:
-      with st.spinner("Parsing file..."):
-        df = pd.read_csv(uploaded_file)
-        num_of_rows = calc_number_rows(df)
-        async with asyncio.TaskGroup() as tg:
-          column_validation = tg.create_task(check_file(df, column_validation))
-          content_validation = tg.create_task(parse_msg_data(df, content_validation))
-          phone_num_validation = tg.create_task(parse_valid_numbers(df, phone_num_validation))
-          dedupe_validation = tg.create_task(parse_dup_numbers(df, dedupe_validation, num_of_rows))
+    with st.spinner("Parsing file..."):
+      df = pd.read_csv(uploaded_file)
+      num_of_rows = calc_number_rows(df)
+      async with asyncio.TaskGroup() as tg:
+        column_validation = tg.create_task(check_file(df, column_validation))
+        content_validation = tg.create_task(parse_msg_data(df, content_validation))
+        phone_num_validation = tg.create_task(parse_valid_numbers(df, phone_num_validation))
+        dedupe_validation = tg.create_task(parse_dup_numbers(df, dedupe_validation, num_of_rows))
+        time.sleep(2)
+      column_validation = column_validation.result()
+      content_validation = content_validation.result()
+      phone_num_validation = phone_num_validation.result()
+      dedupe_validation = dedupe_validation.result()
   else:
     st.write('Please upload a file.')
 
-  if column_validation == False:
-    st.write("Validation failed: Check column headers.")
-  elif content_validation == False:
-    st.write("Validation failed: Check message content.")
-  elif phone_num_validation == False:
-    st.write("Validation failed: Ensure phone numbers are 11 digits and include a country code at the beginning.")
-  elif dedupe_validation == False:
-    st.write("Validation failed: Remove duplicate phone numbers.")
-  else:
+  if column_validation == True and content_validation == True and phone_num_validation == True and dedupe_validation == True:
     validate_disabled = False
     st.write('File validation successful!')
 
